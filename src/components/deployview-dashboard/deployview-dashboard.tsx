94bfb414a21fa76869d9642c865561dcd3cdcf63
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-09-10 22:49:55 +0200

    [#2/export] Header angepasst
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- src/components/deployview-dashboard/deployview-dashboard.tsx
+++ src/components/deployview-dashboard/deployview-dashboard.tsx
@@ -9,7 +9,10 @@ export class AppHome {
     return [
       <ion-header>
         <ion-toolbar color="primary">
-          <ion-title>Home</ion-title>
+          <ion-title>Status Übersicht bekannter Artefakte</ion-title>
+          <ion-buttons slot="secondary">
+            <ion-button size="small" shape="round" color="light" href="/verwaltung/ionic">Verwaltung</ion-button>
+          </ion-buttons>
         </ion-toolbar>
       </ion-header>,
 
@@ -18,10 +21,6 @@ export class AppHome {
           Welcome to the PWA Toolkit. You can use this starter to build entire apps with web components using Stencil and ionic/core! Check out the README for everything that comes
           in this starter out of the box and check out our docs on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
         </p>
-
-        <ion-button href="/verwaltung/ionic" expand="block">
-          Export / Import
-        </ion-button>
       </ion-content>,
     ];
   }
